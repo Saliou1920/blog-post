4dfa01f122170593f8d809446c03641eafcabd23
Saliou <60982664+Saliou1920@users.noreply.github.com>
2022-01-22 11:08:05 -0500

    caching results with redis

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,46 @@
 const axios = require("axios");
+const Redis = require("redis");
 const apiUrl = "https://api.hatchways.io/assessment/blog/posts?";
+
+const redisClient = Redis.createClient();
+const DEFAULT_EXPIRATION_TIME = 3600;
+
+function getOrSetCache(key, callback) {
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, async (err, cached) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (cached) {
+        return resolve(JSON.parse(cached));
+      }
+
+      const result = await callback();
+      redisClient.setex(key, DEFAULT_EXPIRATION_TIME, JSON.stringify(result));
+      return resolve(result);
+    });
+  });
+}
 
 const fetchPosts = async (tag) => {
   const url = `${apiUrl}tag=${tag}`;
-  try {
-    const { data } = await axios.get(url);
+  const posts = await getOrSetCache(`posts?${tag}`, () => {
+    const { data } = axios.get(url);
     return data.posts;
-  } catch (err) {
-    console.error(err);
-  }
+  });
+  return posts;
 };
+
+// const fetchPosts = async (tag) => {
+//   const url = `${apiUrl}tag=${tag}`;
+//   try {
+//     const { data } = await axios.get(url);
+//     return data.posts;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 const getAllPosts = async (req, res) => {
   const { tags, sortBy = "id", direction = "asc" } = req.query;
