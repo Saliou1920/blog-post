ef13021131ea906be3515d5deb78f8989f5800c8
Saliou1920 <diopsaliou45@gmail.com>
2022-01-21 20:37:11 -0400

    adding routes and handling error

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
 require("express-async-errors");
+const posts = require("./routes/posts");
+const notFoundMiddleware = require("./middleware/notFound");
+const errorMiddleware = require("./middleware/errorHandler");
 
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/ping", (req, res) => {
-  res.status(200).send({ suceess: true });
-});
+app.use("/", posts);
+app.use("/api/posts", posts);
+// error handling middleware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
 const port = process.env.PORT || 3000;
 
 const start = async () => {
