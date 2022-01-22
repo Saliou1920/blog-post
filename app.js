1bf0d0a91ca65967a2f743891b6e46808c99af61
Saliou1920 <diopsaliou45@gmail.com>
2022-01-22 17:58:01 -0400

    adding helmet

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("express-async-errors");
+
 const posts = require("./routes/posts");
+
 const notFoundMiddleware = require("./middleware/notFound");
 const errorMiddleware = require("./middleware/errorHandler");
+
+// Security
+const helmet = require("helmet");
 
 const express = require("express");
 const app = express();
@@ -10,9 +15,12 @@ app.use(express.json());
 
 app.use("/", posts);
 app.use("/api/posts", posts);
-// error handling middleware
+
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
+
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
 
 const port = process.env.PORT || 3000;
 
