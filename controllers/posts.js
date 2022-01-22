039447a3ebd824aaaf46f82caf2afc1ac45631fb
Saliou1920 <diopsaliou45@gmail.com>
2022-01-21 20:36:32 -0400

    posts route logic

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,41 @@
-const getAllPosts = async (req, res) => {
-  res.status(200).send("All posts");
+const axios = require("axios");
+const apiUrl = "https://api.hatchways.io/assessment/blog/posts?";
+
+const fetchPosts = async (tag) => {
+  const url = `${apiUrl}tag=${tag}`;
+  try {
+    const { data } = await axios.get(url);
+    return data.posts;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-const getPostByTag = async (req, res) => {
-  const { tag } = req.params;
-  res.status(200).send(`Posts with tag ${tag}`);
+const getAllPosts = async (req, res) => {
+  const { tags, sortBy = "id", direction = "asc" } = req.query;
+
+  if (!tags) {
+    return res.status(400).json({ error: "Missing tags" });
+  }
+
+  if (tags || sortBy) {
+    const posts = await Promise.all(
+      tags.split(",").map((tag) => fetchPosts(tag))
+    );
+    const sortedPosts = posts.flat().sort((a, b) => {
+      if (direction === "asc") {
+        return parseFloat(a[sortBy]) - parseFloat(b[sortBy]);
+      }
+      if (direction === "desc") {
+        return parseFloat(b[sortBy]) - parseFloat(a[sortBy]);
+      } else {
+        return res.status(400).json({ error: "Invalid sort direction" });
+      }
+    });
+    return res.status(200).json({ sortedPosts });
+  }
 };
 
 module.exports = {
   getAllPosts,
-  getPostByTag,
 };
